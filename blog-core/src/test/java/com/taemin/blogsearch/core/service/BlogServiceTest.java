aab2387a6af27bb75303f41c189cd1ebbc0c9ab9
niKe <taminging@kakao.com>
2023-03-22 00:45:50 +0900

    [test] BlogServiceTest test code 리펙토링

--- blog-core/src/test/java/com/taemin/blogsearch/core/service/BlogServiceTest.java
+++ blog-core/src/test/java/com/taemin/blogsearch/core/service/BlogServiceTest.java
@@ -47,75 +47,9 @@ public class BlogServiceTest {
 
     @BeforeEach
     public void beforeEach() {
-        KakaoDocument kakaoDocumentOne = KakaoDocument.builder()
-                .blogName("name")
-                .contents("contents")
-                .title("title")
-                .datetime(new Date())
-                .thumbnail("thumbnail")
-                .url("url")
-                .build();
-
-        KakaoDocument kakaoDocumentTwo = KakaoDocument.builder()
-                .blogName("name")
-                .contents("contents")
-                .title("title")
-                .datetime(new Date())
-                .thumbnail("thumbnail")
-                .url("url")
-                .build();
-
-        NaverItem naverItemOne = NaverItem.builder()
-                .bloggerName("name")
-                .description("description")
-                .link("link")
-                .postDate("20220101")
-                .title("title")
-                .bloggerLink("bloggerLink")
-                .build();
-
-        NaverItem naverItemTwo = NaverItem.builder()
-                .bloggerName("name")
-                .description("description")
-                .link("link")
-                .postDate("20220101")
-                .title("title")
-                .bloggerLink("bloggerLink")
-                .build();
-
-        doReturnKakaoMeta = new KakaoMeta(false, 100, 10000);
-        doReturnKakaoDocuments = new KakaoDocuments(Arrays.asList(kakaoDocumentOne, kakaoDocumentTwo));
-        doReturnNaverItems = new NaverItems(Arrays.asList(naverItemOne, naverItemTwo));
-
-        KeywordSearchCount keywordSearchCountOne = new KeywordSearchCount("1 keyword", 1);
-        KeywordSearchCount keywordSearchCountTwo = new KeywordSearchCount("2 keyword", 2);
-        KeywordSearchCount keywordSearchCountThree = new KeywordSearchCount("4 keyword", 3);
-        KeywordSearchCount keywordSearchCountFour = new KeywordSearchCount("5 keyword", 4);
-        KeywordSearchCount keywordSearchCountFive = new KeywordSearchCount("6 keyword", 5);
-        KeywordSearchCount keywordSearchCountSix = new KeywordSearchCount("7 keyword", 6);
-        KeywordSearchCount keywordSearchCountSeven = new KeywordSearchCount("8 keyword", 7);
-        KeywordSearchCount keywordSearchCountEight = new KeywordSearchCount("9 keyword", 8);
-        KeywordSearchCount keywordSearchCountNine = new KeywordSearchCount("10 keyword", 9);
-        KeywordSearchCount keywordSearchCountTen = new KeywordSearchCount("11 keyword", 10);
-        KeywordSearchCount keywordSearchCountEleven = new KeywordSearchCount("12 keyword", 11);
-        KeywordSearchRank keywordSearchRank = new KeywordSearchRank(Arrays.asList(
-                keywordSearchCountOne,
-                keywordSearchCountTwo,
-                keywordSearchCountThree,
-                keywordSearchCountFour,
-                keywordSearchCountFive,
-                keywordSearchCountSix,
-                keywordSearchCountSeven,
-                keywordSearchCountEight,
-                keywordSearchCountNine,
-                keywordSearchCountTen,
-                keywordSearchCountEleven
-
-        ));
-
-        doReturnKeywordSearchCounts = keywordSearchRank.getKeywordSearchRank();
-
-
+        initKakaoBlog();
+        initNaverBlog();
+        initKeywordSearchCount();
     }
 
     @Test
@@ -162,6 +96,78 @@ public class BlogServiceTest {
         assertThat(keywordSearchCounts.get(7).getKeyword()).isEqualTo("5 keyword");
         assertThat(keywordSearchCounts.get(8).getKeyword()).isEqualTo("4 keyword");
         assertThat(keywordSearchCounts.get(9).getKeyword()).isEqualTo("2 keyword");
+    }
+
+    private void initKakaoBlog() {
+        KakaoDocument kakaoDocumentOne = KakaoDocument.builder()
+                .blogName("name")
+                .contents("contents")
+                .title("title")
+                .datetime(new Date())
+                .thumbnail("thumbnail")
+                .url("url")
+                .build();
+
+        KakaoDocument kakaoDocumentTwo = KakaoDocument.builder()
+                .blogName("name")
+                .contents("contents")
+                .title("title")
+                .datetime(new Date())
+                .thumbnail("thumbnail")
+                .url("url")
+                .build();
+        doReturnKakaoMeta = new KakaoMeta(false, 100, 10000);
+        doReturnKakaoDocuments = new KakaoDocuments(Arrays.asList(kakaoDocumentOne, kakaoDocumentTwo));
+    }
+
+    private void initNaverBlog() {
+        NaverItem naverItemOne = NaverItem.builder()
+                .bloggerName("name")
+                .description("description")
+                .link("link")
+                .postDate("20220101")
+                .title("title")
+                .bloggerLink("bloggerLink")
+                .build();
+
+        NaverItem naverItemTwo = NaverItem.builder()
+                .bloggerName("name")
+                .description("description")
+                .link("link")
+                .postDate("20220101")
+                .title("title")
+                .bloggerLink("bloggerLink")
+                .build();
+        doReturnNaverItems = new NaverItems(Arrays.asList(naverItemOne, naverItemTwo));
+    }
+
+    private void initKeywordSearchCount() {
+        KeywordSearchCount keywordSearchCountOne = new KeywordSearchCount("1 keyword", 1);
+        KeywordSearchCount keywordSearchCountTwo = new KeywordSearchCount("2 keyword", 2);
+        KeywordSearchCount keywordSearchCountThree = new KeywordSearchCount("4 keyword", 3);
+        KeywordSearchCount keywordSearchCountFour = new KeywordSearchCount("5 keyword", 4);
+        KeywordSearchCount keywordSearchCountFive = new KeywordSearchCount("6 keyword", 5);
+        KeywordSearchCount keywordSearchCountSix = new KeywordSearchCount("7 keyword", 6);
+        KeywordSearchCount keywordSearchCountSeven = new KeywordSearchCount("8 keyword", 7);
+        KeywordSearchCount keywordSearchCountEight = new KeywordSearchCount("9 keyword", 8);
+        KeywordSearchCount keywordSearchCountNine = new KeywordSearchCount("10 keyword", 9);
+        KeywordSearchCount keywordSearchCountTen = new KeywordSearchCount("11 keyword", 10);
+        KeywordSearchCount keywordSearchCountEleven = new KeywordSearchCount("12 keyword", 11);
+        KeywordSearchRank keywordSearchRank = new KeywordSearchRank(Arrays.asList(
+                keywordSearchCountOne,
+                keywordSearchCountTwo,
+                keywordSearchCountThree,
+                keywordSearchCountFour,
+                keywordSearchCountFive,
+                keywordSearchCountSix,
+                keywordSearchCountSeven,
+                keywordSearchCountEight,
+                keywordSearchCountNine,
+                keywordSearchCountTen,
+                keywordSearchCountEleven
+
+        ));
+        doReturnKeywordSearchCounts = keywordSearchRank.getKeywordSearchRank();
     }
 
 }
